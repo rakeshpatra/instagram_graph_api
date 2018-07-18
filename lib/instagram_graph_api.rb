5da4493898b8b2b978ce8da6e7bae31d6ddc5391
rakeshp <rakesh.patra@mindfiresolutions.com>
2018-07-18 16:47:40 +0530

    initial commit

---
+++ lib/instagram_graph_api.rb
@@ -0,0 +1,6 @@
+require "instagram_graph_api/version"
+require "instagram_graph_api/client"
+
+module InstagramGraphApi
+  # Your code goes here...
+end
---
+++ lib/instagram_graph_api/version.rb
@@ -0,0 +1,3 @@
+module InstagramGraphApi
+  VERSION = "0.1.0"
+end
