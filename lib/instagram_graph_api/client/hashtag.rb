fd180b23171aa71a453441ec3e78482148093096
mybys <miyabayashi88@gmail.com>
2019-12-17 14:07:46 +0900

    add paging option to get_hashtag_recent_media

--- lib/instagram_graph_api/client/hashtag.rb
+++ lib/instagram_graph_api/client/hashtag.rb
@@ -9,6 +9,8 @@ module InstagramGraphApi
       def get_hashtag_recent_media(hashtag_id, ig_account_id, fields = nil, options: {})
         fields ||= "caption,children,comments_count,id,like_count,media_type,media_url,permalink"
         query = "recent_media?user_id=#{ig_account_id}&fields=#{fields}"
+        query += "&after=#{options[:after]}" if options[:after]
+        query += "&before=#{options[:before]}" if options[:before]
         query += "&limit=#{options[:limit]}" if options[:limit]
         get_connections(hashtag_id, query)
       end
